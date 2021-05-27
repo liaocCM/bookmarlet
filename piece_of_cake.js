1de7873de90a8f20069d08a07bf9edb0390306c1
Chien-Ming Liao <linex1008@gmail.com>
2021-05-27 22:38:29 +0800

    Update piece_of_cake.js

--- piece_of_cake.js
+++ piece_of_cake.js
@@ -1,13 +1,20 @@
-// full version
-javascript:(function(){
-  var info = ['廖建銘', 'Tex Liao', '36.1'];
-  document.getElementsByTagName('input').forEach((e, i) => {e.value = info[i]});
+// devtools console
+(function(){
+  const info = ['廖建銘', 'Tex Liao', '36'];
+  const ff = (regex) => Array.from(document.querySelectorAll('span')).filter(e => regex.test(e.innerText)).forEach((e, i) => !(i % 2) && e.children[0].click());
+  document.querySelectorAll('input').forEach((e, i) => e.value = info[i]);
   [/^2021\//,/R&D/,/在家工作/,/無不適/,/否/].forEach(e => ff(e));
-  document.getElementsByTagName('button')[0].click();
-  function ff(regex){
-    return Array.from(document.querySelectorAll('span')).filter(e => regex.test(e.innerText)).forEach((e, i) => !(i % 2) && e.children[0].click());
-  }
+  document.querySelectorAll('button')[0].click();  
 })()
 
-// oneline version
+// bookmarlet full version
+javascript:(function(){
+  const info = ['廖建銘', 'Tex Liao', '36'];
+  const ff = (regex) => Array.from(document.querySelectorAll('span')).filter(e => regex.test(e.innerText)).forEach((e, i) => !(i % 2) && e.children[0].click());
+  document.querySelectorAll('input').forEach((e, i) => e.value = info[i]);
+  [/^2021\//,/R&D/,/在家工作/,/無不適/,/否/].forEach(e => ff(e));
+  document.querySelectorAll('button')[0].click();  
+})()
 
+// bookmarlet oneline version
+javascript:!function(){const e=["廖建銘","Tex Liao","36"];document.querySelectorAll("input").forEach((c,r)=>c.value=e[r]),[/^2021\//,/R&D/,/在家工作/,/無不適/,/否/].forEach(e=>(e=>Array.from(document.querySelectorAll("span")).filter(c=>e.test(c.innerText)).forEach((e,c)=>!(c%2)&&e.children[0].click()))(e)),document.querySelectorAll("button")[0].click()}();
